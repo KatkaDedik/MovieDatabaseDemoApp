9c5e4fb8f5f8c623c49855998bf5d413b85a5376
katka <dedikovakatka@gmail.com>
2025-10-20 14:00:56 +0200

    ADD: Application layer and DTOs (ActorDto, MovieDto)

---
+++ Application/DTOs/ActorDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalHomework.Application.DTOs
+{
+    public record ActorDto(int Id, string Name, DateTime BirthDate);
+}
---
+++ Application/DTOs/MovieDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalHomework.Application.DTOs
+{
+    public record MovieDto(int Id, string Title, string Genre, float Rating, int Year, List<int> ActorsIds);
+}
