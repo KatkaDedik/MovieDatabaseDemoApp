c3ccbdfd1b70600407040ab8527dc5992a6b2864
katka <dedikovakatka@gmail.com>
2025-10-20 13:41:23 +0200

    FIX: move Domain into correct folder

---
+++ Domain/Entities/Actor.cs
@@ -0,0 +1,9 @@
+﻿namespace AdditionalHomework.Domain.Entities
+{
+    public class Actor
+    {
+        int Id { get; set; }
+        string Name { get; set; } = string.Empty;
+        DateOnly BirthDate { get; set; }
+    }
+}
---
+++ Domain/Entities/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace AdditionalHomework.Domain.Entities
+{
+    public class Movie
+    {
+        int Id { get; set; }
+        string Title { get; set; } = string.Empty;
+        string Genre { get; set; } = string.Empty;
+        float Rating { get; set; }
+        int Year { get; set; }
+        List<int> ActorsIds { get; set; } = new();
+    }
+}
