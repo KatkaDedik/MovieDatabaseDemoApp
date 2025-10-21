11fa13a1ca909a57bc06aafcd0e3520526f87cc9
katka <dedikovakatka@gmail.com>
2025-10-21 10:53:14 +0200

    ADD: Actor extension get age

---
+++ Domain/Extensions/ActorExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApp.Domain.Extensions
+{
+    public static class ActorExtension
+    {
+        public static int GetAge(this DateTime birthDate)
+        {
+            var today = DateTime.Today;
+            var age = today.Year - birthDate.Year;
+            if (birthDate.Date > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
