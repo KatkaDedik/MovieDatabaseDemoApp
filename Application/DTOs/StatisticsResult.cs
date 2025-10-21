ad5c7ad6cd4225d779b62e9a64d025a1cbf8add0
katka <dedikovakatka@gmail.com>
2025-10-21 10:46:41 +0200

    ADD: statistics result record

---
+++ Application/DTOs/StatisticsResult.cs
@@ -0,0 +1,15 @@
+﻿using MovieApp.Domain.Entities;
+
+namespace MovieApp.Application.DTOs
+{
+    public record StatisticsResult(
+        int TotalMovies,
+        int TotalActors,
+        decimal AverageRating,
+        List<string> TopGenres,
+        int OldestActorAge,
+        int YoungestActorAge,
+        string MostProlificActor,
+        decimal HighestRatedGenreAvg,
+        Movie? LongestTitleMovie);
+}
